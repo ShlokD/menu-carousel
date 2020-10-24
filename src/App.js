152d2e52b9460e0c22e8a43b9c9ab938283cd316
Shlok Desai <shlokpdesai@gmail.com>
2020-10-24 13:13:32 -0700

    implement carousel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import images from "./images";
+
+const Carousel = ({ images }) => {
+  const [selectedImage, setSelectedImage] = useState("");
+
+  const toggleElem = (name) => {
+    setSelectedImage(name);
+  };
+
+  const showElem = (name) => {
+    const elemId = `carousel-item-${name}`;
+    const elem = document.getElementById(elemId);
+    elem.scrollIntoView({ behaviour: "smooth", block: "center" });
+  };
+
+  return (
+    <div class="flex">
+      <div className="w-75 flex flex-column items-center">
+        {images.map((image, index) => {
+          return (
+            <img
+              key={`carousel-item-${index}`}
+              alt={image.name}
+              id={`carousel-item-${image.name}`}
+              className="ma4 pa4 h-75 w-80 shadow-2"
+              src={image.src}
+              height="75%"
+              width="80%"
+              style={{
+                transform: `rotateX(${
+                  selectedImage === image.name ? "0deg" : "45deg"
+                })`,
+                transition: "transform 0.5s ease-in-out",
+                perspective: "800px",
+              }}
+            ></img>
+          );
+        })}
+      </div>
+      <div className="w-25 h-100 fixed right-0 flex flex-column items-start justify-center">
+        {images.map(({ name }, index) => {
+          return (
+            <div
+              key={`list-item-${index}`}
+              onMouseEnter={() => showElem(name)}
+              onChange={() => toggleElem(name)}
+              className="dib ma2 pa4 pointer tc"
+            >
+              <input
+                className="dn"
+                type="radio"
+                id={name}
+                name="menu-item"
+              ></input>
+              <label className="pointer underline f4" htmlFor={name}>
+                {name}
+              </label>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel images={images} />
     </div>
   );
 }
---
+++ src/images.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: "Day",
+    src:
+      "https://images.unsplash.com/photo-1603472389867-97e05566ecc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
+  },
+  {
+    name: "Inspire",
+    src:
+      "https://images.unsplash.com/photo-1603543394745-9d100843680b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+  },
+  {
+    name: "Opinion",
+    src:
+      "https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
+  },
+  {
+    name: "Jewel",
+    src:
+      "https://images.unsplash.com/photo-1603473036383-df544a1dcada?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1328&q=80",
+  },
+  {
+    name: "Migration",
+    src:
+      "https://images.unsplash.com/photo-1603538794058-7471d7648902?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+  },
+  {
+    name: "Arrow",
+    src:
+      "https://images.unsplash.com/photo-1603536764976-e2d8a6d805fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1367&q=80",
+  },
+];
